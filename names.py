cc745eab1adc2caf82469124a1eeeb4726a788e8
Arnav Moudgil <ocean57@gmail.com>
2012-02-21 12:04:52 -0800

    Update names.py

--- names.py
+++ names.py
@@ -1,4 +1,7 @@
-# Write a program that takes in a (space-delimited) list of names and reports to the user, "You entered these names:" followed by the names (one per line) in alphabetical order. The last line should tell the user how many names they entered, i.e. "You entered ____ names."
+# Write a program that takes in a (space-delimited) list of names
+# and reports to the user, "You entered these names:" 
+# followed by the names (one per line) in alphabetical order. 
+# The last line should tell the user how many names they entered, i.e. "You entered ____ names."
 
 names = raw_input("Enter a list of names separated by a single space: ") # Helpful greeting message
 names = names.strip().split(' ') # Eliminate any leading and trailing whitespace characters
