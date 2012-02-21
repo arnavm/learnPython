5f2fbb2ddf47eba736f455a42136624fa87410cf
Arnav Moudgil <ocean57@gmail.com>
2012-02-21 12:05:58 -0800

    Update tipCalculator.py

--- tipCalculator.py
+++ tipCalculator.py
@@ -1,4 +1,6 @@
-# Write a program that takes as input the cost of a meal. Then, tell the user what 15% tip would be ("Fifteen percent tip for this meal is ____") and what the total cost will be ("This meal costs ___ in total").
+# Write a program that takes as input the cost of a meal. 
+# Then, tell the user what 15% tip would be ("Fifteen percent tip for this meal is ____") 
+# and what the total cost will be ("This meal costs ___ in total").
 
 cost = float(raw_input("Cost of your meal: ")) # Convert the input string to a float
 print "Fifteen percent tip for this meal is $%.2f" % (cost * 0.15) # Calculate and print the tip
